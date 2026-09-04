d8de5c20c8eef86f750063ea61ccc040a1b36906
chuplo30 <wandascr2025@gmail.com>
2026-09-04 14:34:27 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3069,7 +3069,7 @@ def _health():
     return "Bot is running."
 
 def _run_keep_alive():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     keep_alive_app.run(host="0.0.0.0", port=port)
 
 def start_keep_alive():
